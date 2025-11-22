cb19cb24b825b95f5d2f3089a01cde0929cbee9d
GauravShakya0 <96975111+GauravShakya0@users.noreply.github.com>
2025-11-22 12:28:18 +0530

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "TechNova Software Solutions",
+  title: "Bankai Software Solutions",
   description: "Next-gen software development for modern businesses.",
 };
 
